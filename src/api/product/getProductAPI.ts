95830ef6cf5b013c8d2406215df822a7246e5e9e
seokhyungth <korea970223@naver.com>
2023-08-09 20:17:03 +0900

    :sparkles:Feat : 상품 불러오는 API 함수 구현

---
+++ src/api/product/getProductAPI.ts
@@ -0,0 +1,13 @@
+export const getProductAPI = async () => {
+  try {
+    const response = await fetch('https://openmarket.weniv.co.kr/products/');
+
+    if (!response.ok) {
+      throw new Error('네트워크에 문제가 있습니다.');
+    }
+    const data = await response.json();
+    return data.results;
+  } catch (error) {
+    console.error('상품 데이터를 가져오는데 문제가 있습니다.', error);
+  }
+};
