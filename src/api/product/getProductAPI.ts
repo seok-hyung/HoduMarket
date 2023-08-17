79f424a2e99f109f0618b8cc0e5ec2127d40c43b
seokhyungth <korea970223@naver.com>
2023-08-17 14:19:23 +0900

    :gear:Chore : styled-reset 설치 #1

--- src/api/product/getProductAPI.ts
+++ src/api/product/getProductAPI.ts
@@ -6,7 +6,7 @@ export const getProductAPI = async () => {
       throw new Error('네트워크에 문제가 있습니다.');
     }
     const data = await response.json();
-    return data.results;
+    return data;
   } catch (error) {
     console.error('상품 데이터를 가져오는데 문제가 있습니다.', error);
   }
--- src/index.tsx
+++ src/index.tsx
@@ -2,10 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
+import GlobalStyled from 'styles/Globalstyled';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
   <BrowserRouter>
+    <GlobalStyled />
     <App />
   </BrowserRouter>,
 );
