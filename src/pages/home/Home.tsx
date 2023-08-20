cc7d4b4d6ad23c8e188397557c6d4c6728ab1130
seokhyungth <korea970223@naver.com>
2023-08-20 19:08:18 +0900

    :sparkles:Modify : Home 페이지에 Footer 컴포넌트 추가 #4

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,15 +1,19 @@
 import React, { useEffect, useState } from 'react';
 
-import Nav from 'components/common/nav/Nav';
-import Carousel from 'components/common/carousel/Carousel';
-
 // import { Route, Routes } from 'react-router-dom';
 import { ProductResults } from 'model/market';
 import { getProductAPI } from '../../api/product/getProductAPI';
 
+// 컴포넌트
+import Nav from 'components/common/nav/Nav';
+import Carousel from 'components/common/carousel/Carousel';
+import Products from 'components/common/products/Products';
+import Footer from 'components/common/footer/Footer';
+
 const Home: React.FC = () => {
   const [products, setProducts] = useState<ProductResults[]>([]);
   const productImg = products.map((obj) => obj?.image);
+  console.log(products);
 
   useEffect(() => {
     getProductAPI()
@@ -25,6 +29,8 @@ const Home: React.FC = () => {
     <>
       <Nav />
       <Carousel images={productImg} />
+      <Products goods={products} />
+      <Footer />
     </>
   );
 };
