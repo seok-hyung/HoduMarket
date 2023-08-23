56d5c950bbcc687502510fafe73c1107e721b6ae
seokhyungth <korea970223@naver.com>
2023-08-23 15:56:21 +0900

    :truck:Modify : 상세페이지 Route 추가

--- src/pages/Pages.tsx
+++ src/pages/Pages.tsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
+
 import Home from './home/Home';
+import ProductDetail from './productDeatil/ProductDetail';
 
 function Pages() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
+      <Route path="/detail/:id" element={<ProductDetail />} />
     </Routes>
   );
 }
