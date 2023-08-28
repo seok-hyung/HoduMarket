c9c08351c35b0ea9ac1742efd81b2bac0b8920e9
seokhyungth <korea970223@naver.com>
2023-08-28 21:20:46 +0900

    :truck:Modify : 이미지 경로 재설정

--- src/components/common/nav/Nav.tsx
+++ src/components/common/nav/Nav.tsx
@@ -13,20 +13,24 @@ const Nav = () => {
             onClick={() => {
               navigate('/');
             }}
-            src={'/assets/Logo-hodu.png'}
+            src="/assets/Logo-hodu.png"
             alt="메인로고"
           />
           <S.SearchInput type="text" placeholder="상품을 검색해보세요!" />
-          <S.SearchImg src={'/assets/icon-search.svg'} alt="검색 이미지" />
+          <S.SearchImg src="/assets/icon-search.svg" alt="검색 이미지" />
         </S.LogoDiv>
 
         <S.NavListUl className="nav-right-list">
           <S.NavListLi>
-            <S.NavListImg src={'/assets/icon-shopping-cart.svg'} alt="장바구니 이미지" />
+            <S.NavListImg src="/assets/icon-shopping-cart.svg" alt="장바구니 이미지" />
             <S.NavListP>장바구니</S.NavListP>
           </S.NavListLi>
-          <S.NavListLi>
-            <S.NavListImg src={'/assets/icon-user.svg'} alt="유저 이미지" />
+          <S.NavListLi
+            onClick={() => {
+              navigate('/login');
+            }}
+          >
+            <S.NavListImg src="/assets/icon-user.svg" alt="유저 이미지" />
             <S.NavListP>로그인</S.NavListP>
           </S.NavListLi>
         </S.NavListUl>
