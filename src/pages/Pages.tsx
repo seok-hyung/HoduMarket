b0e299954791af96fdc4058fcdec6dc915cac107
seokhyungth <korea970223@naver.com>
2023-08-28 21:17:27 +0900

    :truck:Add : 로그인 페이지, 회원가입 페이지 라우트 추가

--- src/pages/Pages.tsx
+++ src/pages/Pages.tsx
@@ -3,12 +3,17 @@ import { Routes, Route } from 'react-router-dom';
 
 import Home from './home/Home';
 import ProductDetail from './productDeatil/ProductDetail';
+import ShopCart from 'components/shopCart/ShopCart';
+import Login from './login/Login';
+import Join from './join/Join';
 
 function Pages() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/detail/:id" element={<ProductDetail />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/join" element={<Join />} />
     </Routes>
   );
 }
