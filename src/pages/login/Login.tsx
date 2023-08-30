55c4776431db6cbf5b83985f5e1647aeba13e5dd
seokhyungth <korea970223@naver.com>
2023-08-30 21:05:23 +0900

    :lipstick:Style : 로그인 페이지 스타일 수정 #7

--- src/pages/login/Login.tsx
+++ src/pages/login/Login.tsx
@@ -1,84 +1,9 @@
+import MemberType from 'components/common/memberType/MemberType';
 import React, { ChangeEvent, FormEvent, useState } from 'react';
-import { SelectedType} from 'model/market';
-import styled  from 'styled-components';
-
-const WrapperDiv = styled.div`
-  max-width: 500px;
-  margin: 180px auto;
-  img {
-    display: block;
-    width: 250px;
-    margin: 0 auto 70px auto;
-  }
-`;
-
-const LoginContainerDiv = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-
-  .login-memberType-box {
-    display: flex;
-  }
-`;
-
-const RoleBtn = styled.button<SelectedType>`
-  width: 275px;
-  padding: 20px;
-  border-top-left-radius: 10px;
-  border-top-right-radius: 10px;
-  border: 1px solid #C4C4C4;
-
-  &:first-child{border-right: 2px solid #C4C4C4};
-  font-size: 18px;
-  line-height: 22px;
-  font-weight: 700;
-  
-  border-bottom: ${(props) => (props.selected ? 'none' : '1px solid #C4C4C4')};
-  background-color: ${(props)=>(props.selected ? '#FFF' : '#F2F2F2')};
-`;
-const LoginForm = styled.form`
-  width: 550px;
-  padding: 30px;
-  padding-bottom:0;
-  border: 1px solid #C4C4C4;
-  border-top: 0;
-  div label{
-    display: block;
-    font-size: 18px;
-    margin: 20px 0;
-    color:var(--sub-text-color);
-  }
-  div input{
-    width: 100%;
-    font-size: 18px;
-    outline: none;
-    border: none;
-    border-bottom: 1px solid var(--main-disabled-color);
-  }
-  .login-btn{
-    margin: 36px 0;
-    background-color: var(--main-color);
-    color: white;
-    font-size: 18px;
-    font-weight: 700;
-    line-height: 22px;
-    border-radius: 5px;
-    padding: 19px 215px;
-  }
-`
-const JoinGroupDiv = styled.div`
-  color:#333333;
-  margin-top: 30px;
-  a:first-child::after{
-    content: '';
-    border-right: 1px solid #333333;
-    margin: 0 14px;
-  }
-`
+import * as S from './Login.style';
+import InputBox from 'components/common/inputBox/InputBox';
 
 const Login = () => {
-  const [selectedRole, setSelectedRole] = useState('BUYER');
   const [loginState, setLoginState] = useState({
     id: '',
     password: '',
@@ -91,76 +16,53 @@ const Login = () => {
       [e.target.name]: e.target.value,
     });
   };
-  const handleRoleChange = (role:'BUYER'|'SELLER') => {
-    setLoginState(prevState=>({...prevState,role}));
-    setSelectedRole(role);
-  };
+
   const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-
     console.log(loginState);
-
     // 서버로 로그인 요청을 전송하거나 다른 처리를 여기서 수행하면 됩니다.
   };
   return (
-    <WrapperDiv>
+    <S.WrapperDiv>
       {/* Logo Image */}
       <img src="/assets/Logo-hodu.png" alt="Logo" />
-      <LoginContainerDiv>
+      <S.LoginContainerDiv>
         {/* Member Type Tabs */}
-        <div className="login-memberType-box">
-          <RoleBtn
-            onClick={()=>handleRoleChange('BUYER')}
-            selected={selectedRole === 'BUYER'}
-          >
-            구매회원 로그인
-          </RoleBtn>
-          <RoleBtn
-            onClick={()=>handleRoleChange('SELLER')}
-            selected={selectedRole === 'SELLER'}
-          >
-            판매회원 로그인
-          </RoleBtn>
-        </div>
+        <MemberType buyerBtnText="구매회원 로그인" sellerBtnText="판매회원 로그인" />
 
         {/* Login Form */}
-        <LoginForm onSubmit={handleSubmit}>
-          <div>
-            <label htmlFor="id">아이디</label>
-            <input
-              type="text"
-              name="id"
-              value={loginState.id}
-              onChange={handleInputChange}
-              required
-            />
-          </div>
-
-          <div>
-            <label htmlFor="password">비밀번호</label>
-            <input
-              type="password"
-              name="password"
-              value={loginState.password}
-              onChange={handleInputChange}
-              required
-            />
-          </div>
+        <S.LoginForm onSubmit={handleSubmit}>
+          <InputBox
+            label="아이디"
+            id="id"
+            name="id"
+            type="text"
+            value={loginState.id}
+            onChange={handleInputChange}
+            required={true}
+          />
+          <InputBox
+            label="비밀번호"
+            name="password"
+            id="password"
+            type="password"
+            value={loginState.password}
+            onChange={handleInputChange}
+            required={true}
+          />
 
           {/* Submit Button */}
-          <button className='login-btn'>로그인</button>
-        </LoginForm>
+          <button className="login-btn">로그인</button>
+        </S.LoginForm>
 
         {/* Links to Registration and Password Recovery */}
-        <JoinGroupDiv>
+        <S.JoinGroupDiv>
           <a href="/join">회원가입</a>
           <a href="/find-password">비밀번호 찾기</a>
-        </JoinGroupDiv>
-      </LoginContainerDiv>
-    </WrapperDiv>
+        </S.JoinGroupDiv>
+      </S.LoginContainerDiv>
+    </S.WrapperDiv>
   );
 };
 
 export default Login;
-
-
