cf4288db8d33df6000750f46f5f3167ebc8d96ae
seokhyungth <korea970223@naver.com>
2023-09-13 20:55:43 +0900

    :sparkles:Feat : 최상단에 RecoilRoot 설정

--- src/index.tsx
+++ src/index.tsx
@@ -4,15 +4,18 @@ import App from './App';
 import { BrowserRouter } from 'react-router-dom';
 import GlobalStyled from 'styles/Globalstyled';
 import { QueryClientProvider, QueryClient } from 'react-query';
+import { RecoilRoot } from 'recoil';
 
 const queryClient = new QueryClient();
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
-  <BrowserRouter>
-    <GlobalStyled />
-    <QueryClientProvider client={queryClient}>
-      <App />
-    </QueryClientProvider>
-  </BrowserRouter>,
+  <RecoilRoot>
+    <BrowserRouter>
+      <GlobalStyled />
+      <QueryClientProvider client={queryClient}>
+        <App />
+      </QueryClientProvider>
+    </BrowserRouter>
+  </RecoilRoot>,
 );
