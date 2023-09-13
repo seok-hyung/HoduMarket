61807ca432a867373f9039b6b9bf3cdb2d206470
seokhyungth <korea970223@naver.com>
2023-09-13 21:00:06 +0900

    :sparkles:Feat : 로그인토큰 유무에 따른 컴포넌트 설정

--- src/components/common/nav/Nav.tsx
+++ src/components/common/nav/Nav.tsx
@@ -1,10 +1,15 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 
 import * as S from './Nav.style';
+import { useRecoilValue } from 'recoil';
+import { userTokenState } from 'atoms/Atoms';
+import MyPageModal from 'components/modal/MyPageModal';
 
 const Nav = () => {
   const navigate = useNavigate();
+  const userToken = useRecoilValue(userTokenState);
+  const [isModalOpen, setModalOpen] = useState(false);
   return (
     <S.WrapperDiv className="wrapper-nav">
       <S.WrapperNav>
@@ -25,14 +30,22 @@ const Nav = () => {
             <S.NavListImg src="/assets/icon-shopping-cart.svg" alt="장바구니 이미지" />
             <S.NavListP>장바구니</S.NavListP>
           </S.NavListLi>
-          <S.NavListLi
-            onClick={() => {
-              navigate('/login');
-            }}
-          >
-            <S.NavListImg src="/assets/icon-user.svg" alt="유저 이미지" />
-            <S.NavListP>로그인</S.NavListP>
-          </S.NavListLi>
+          {userToken ? (
+            <S.NavListLi onClick={() => setModalOpen(!isModalOpen)}>
+              <S.NavListImg src="/assets/icon-user.svg" alt="로그인된 유저 이미지" />
+              <S.NavListP>마이페이지</S.NavListP>
+            </S.NavListLi>
+          ) : (
+            <S.NavListLi
+              onClick={() => {
+                navigate('/login');
+              }}
+            >
+              <S.NavListImg src="/assets/icon-user.svg" alt="유저 이미지" />
+              <S.NavListP>로그인</S.NavListP>
+            </S.NavListLi>
+          )}
+          {isModalOpen && <MyPageModal />}
         </S.NavListUl>
       </S.WrapperNav>
     </S.WrapperDiv>
