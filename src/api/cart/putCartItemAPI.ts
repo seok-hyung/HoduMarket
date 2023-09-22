0ef4e480e7210a6350e3e5bedc3f91c4bc665595
seokhyungth <korea970223@naver.com>
2023-09-22 21:38:14 +0900

    :sparkles:Feat : 장바구니 상품 수정 API 추가

---
+++ src/api/cart/putCartItemAPI.ts
@@ -0,0 +1,19 @@
+import { apiURL } from 'api/apiURL';
+import { PutCartItemProps } from 'model/market';
+
+export async function putCartItem({ token, urlId, orderData }: PutCartItemProps) {
+  try {
+    const response = await fetch(`${apiURL}/cart/${urlId}/`, {
+      method: 'PUT',
+      headers: {
+        Authorization: `JWT ${token}`,
+        'Content-type': 'application/json',
+      },
+      body: JSON.stringify(orderData),
+    });
+    const data = await response.json();
+    console.log(data);
+  } catch (error) {
+    console.log('데이터를 가져오는데 문제가 생겼습니다.', error);
+  }
+}
