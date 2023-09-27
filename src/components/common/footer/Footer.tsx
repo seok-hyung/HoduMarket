29cf85643a576e0ff912d37259ce42525852ef3a
seokhyungth <korea970223@naver.com>
2023-09-27 20:42:53 +0900

    :lipstick:Modify : 따로 나눴던 css 파일 합침

--- src/components/common/footer/Footer.tsx
+++ src/components/common/footer/Footer.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
-import * as S from './Footer.style';
+import styled from 'styled-components';
 
 const Footer = () => {
   return (
-    <S.WrapperFooter>
-      <S.ContainerDiv>
-        <S.LinkSection>
-          <S.LinkUl>
+    <WrapperFooter>
+      <div className="container-box">
+        <LinkSection>
+          <ul className="link-ul">
             <li>
               <a href="#">호두샵 소개</a>
             </li>
@@ -25,8 +25,8 @@ const Footer = () => {
             <li>
               <a href="#">제휴문의</a>
             </li>
-          </S.LinkUl>
-          <S.LinkBtnUl>
+          </ul>
+          <ul className="link-btn-ul">
             <li>
               <a href="#">
                 <img src={'/assets/icon-insta.svg'} alt="인스타 이미지" />
@@ -42,19 +42,78 @@ const Footer = () => {
                 <img src={'/assets/icon-yt.svg'} alt="유튜브 이미지" />
               </a>
             </li>
-          </S.LinkBtnUl>
-        </S.LinkSection>
+          </ul>
+        </LinkSection>
 
-        <S.AboutSection>
+        <AboutSection>
           <strong>(주)HODU SHOP</strong>
           <br />
           제주특별자치도 제주시 동광고 137 제주코딩베이스캠프 <br />
           사업자 번호 : 000-0000-0000 | 통신판매업
           <br />
           대표 : 김호두
-        </S.AboutSection>
-      </S.ContainerDiv>
-    </S.WrapperFooter>
+        </AboutSection>
+      </div>
+    </WrapperFooter>
   );
 };
 export default Footer;
+
+const WrapperFooter = styled.footer`
+  background-color: #f2f2f2;
+  padding: 40px 320px;
+  .container-box {
+    max-width: 1800px;
+    font-size: 14px;
+    font-weight: 400;
+    margin: 0 auto;
+  }
+`;
+const LinkSection = styled.section`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin-bottom: 22px;
+  .link-ul {
+    display: flex;
+    color: black;
+    min-width: fit-content;
+    gap: 14px;
+    font-size: 20px;
+    li {
+      padding: 0 14px 0 0;
+      position: relative;
+      min-width: fit-content;
+
+      &:not(:last-child)::after {
+        content: '';
+        position: absolute;
+        right: 0;
+        top: 0;
+        bottom: 0;
+        border-right: 1px solid black;
+      }
+    }
+  }
+  .link-btn-ul {
+    display: flex;
+    gap: 14px;
+    img {
+      width: 40px;
+      height: 40px;
+    }
+  }
+`;
+
+const AboutSection = styled.section`
+  border-top: 1px solid #c4c4c4;
+  padding-top: 20px;
+  font-size: 20px;
+  letter-spacing: 3px;
+  color: #767676;
+  line-height: 24px;
+
+  strong {
+    font-weight: 700;
+  }
+`;
