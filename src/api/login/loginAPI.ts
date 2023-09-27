ffbc1ea510ce76766d38b27dd7f3b0693798f584
seokhyungth <korea970223@naver.com>
2023-09-27 20:40:55 +0900

    :truck:Modify : 로그인api 위치 수정

--- src/api/login/loginAPI.ts
+++ src/api/login/loginAPI.ts
@@ -1,7 +1,7 @@
 import { apiURL } from 'api/apiURL';
 import { LoginForm } from 'model/market';
 
-export const LoginAPI = async (formData: LoginForm) => {
+export const loginAPI = async (formData: LoginForm) => {
   const res = await fetch(`${apiURL}/accounts/login/`, {
     method: 'POST',
     body: JSON.stringify(formData),
@@ -10,11 +10,8 @@ export const LoginAPI = async (formData: LoginForm) => {
     },
   });
   if (!res.ok) {
-    const errorData = await res.json();
-    console.error(errorData);
     throw new Error('Network response was not ok');
   }
   const data = await res.json();
-  console.log(data);
   return data;
 };
