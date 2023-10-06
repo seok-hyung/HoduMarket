3ce0257cc168730e1b7e42f6720269dcf683ec5d
seokhyungth <korea970223@naver.com>
2023-10-06 23:17:30 +0900

    :sparkles:Feat : 상품의 id로 세부 데이터 가져오는 api 추가

---
+++ src/api/product/getDetailProductAPI.ts
@@ -0,0 +1,21 @@
+import { apiURL } from 'api/apiURL';
+import React from 'react';
+
+export const getDetailProductAPI = async (product_id: number) => {
+  try {
+    const res = await fetch(`${apiURL}/products/${product_id}/`, {
+      method: 'GET',
+      headers: {
+        'Content-type': 'application/json',
+      },
+    });
+    if (!res.ok) {
+      throw new Error('네트워크에 문제가 있습니다.');
+    }
+    const data = await res.json();
+    return data;
+  } catch (error) {
+    console.error('디테일 상품 데이터를 가져오는데 문제가 있습니다.', error);
+    throw error;
+  }
+};
