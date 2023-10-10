1212e0bf2c6a2e2bb4a9be0cb1b6b85e73484e06
seokhyungth <korea970223@naver.com>
2023-10-10 22:57:28 +0900

    :sparkles:Feat : 함수 인자 타입 수정

--- src/api/cart/putCartItemAPI.ts
+++ src/api/cart/putCartItemAPI.ts
@@ -1,19 +1,30 @@
 import { apiURL } from 'api/apiURL';
 import { PutCartItemForm } from 'model/market';
 
-export async function putCartItem({ token, urlId, orderData }: PutCartItemForm) {
+export async function putCartItemAPI(
+  token: string,
+  cart_item_id: number | string | undefined,
+  formData: PutCartItemForm,
+) {
   try {
-    const response = await fetch(`${apiURL}/cart/${urlId}/`, {
+    const res = await fetch(`${apiURL}/cart/${cart_item_id}/`, {
       method: 'PUT',
       headers: {
         Authorization: `JWT ${token}`,
         'Content-type': 'application/json',
       },
-      body: JSON.stringify(orderData),
+      body: JSON.stringify(formData),
     });
-    const data = await response.json();
+    if (!res.ok) {
+      // Not a successful HTTP response, log the text body
+      const text = await res.text();
+      console.error('Server error:', text);
+
+      return;
+    }
+    const data = await res.json();
     console.log(data);
   } catch (error) {
-    console.log('데이터를 가져오는데 문제가 생겼습니다.', error);
+    console.log('장바구니 상품 업데이트하는데 실패했습니다.', error);
   }
 }
