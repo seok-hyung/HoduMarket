e7d10c090167cbb82629e7d2effa470e004f13fc
seokhyungth <korea970223@naver.com>
2023-10-11 15:03:15 +0900

    :recycle:Refactor : Pages 라우터 역할을 App 컴포넌트가 하게끔 수정

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,23 @@
 import React from 'react';
-import Pages from './pages/Pages';
+import { Route, Routes } from 'react-router-dom';
+import Home from 'pages/Home';
+import ProductDetail from 'pages/ProductDetail';
+import Login from 'pages/Login';
+import Join from 'pages/Join';
+import Cart from 'pages/Cart';
+import Page404 from 'pages/Page404';
 
 function App() {
-  return <Pages />;
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/detail/:id" element={<ProductDetail />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/join" element={<Join />} />
+      <Route path="/cart" element={<Cart />} />
+      <Route path="/page404" element={<Page404 />} />
+    </Routes>
+  );
 }
 
 export default App;
