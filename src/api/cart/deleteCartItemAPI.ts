b0993c0c497ce13bda58878f89d009b2116bba67
seok-hyung <korea970223@naver.com>
2023-10-23 23:00:37 +0900

    :bug:Modify : headers에 Autorization 추가

--- src/api/cart/deleteCartItemAPI.ts
+++ src/api/cart/deleteCartItemAPI.ts
@@ -1,7 +1,13 @@
 import { apiURL } from 'api/apiURL';
 
 export const deleteCartItemAPI = async (token: string, cart_item_id: number) => {
-  const res = await fetch(`${apiURL}/cart/${cart_item_id}/`, { method: 'DELETE' });
+  const res = await fetch(`${apiURL}/cart/${cart_item_id}/`, {
+    method: 'DELETE',
+    headers: {
+      Authorization: `JWT ${token}`,
+      'Content-type': 'application/json',
+    },
+  });
 
   if (!res.ok) {
     throw new Error('Network response was not ok');
