e7d9b87dc2c0b92516c7e3131efce4174435da7a
seok-hyung <korea970223@naver.com>
2023-10-31 21:00:35 +0900

    :recycle:Refactor : Nav 요소 랜더링 함수 구현 #24

--- src/components/common/nav/Nav.tsx
+++ src/components/common/nav/Nav.tsx
@@ -1,51 +1,88 @@
 import React, { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { styled } from 'styled-components';
-import { useRecoilState } from 'recoil';
-import { userTokenState } from 'atoms/Atoms';
+import { useRecoilState, useRecoilValue } from 'recoil';
+import { userTokenState, userTypeState } from 'atoms/Atoms';
 import MyPageModal from 'components/modal/MyPageModal';
+
+const MenuItem = ({ onClick, src, alt, text }: any) => (
+  <li className="listLi" onClick={onClick}>
+    <img className="navImg" src={src} alt={alt} />
+    <p>{text}</p>
+  </li>
+);
 
 const Nav = () => {
   const navigate = useNavigate();
   const [userToken, setUserToken] = useRecoilState(userTokenState);
+  const userType = useRecoilValue(userTypeState);
   const [isModalOpen, setModalOpen] = useState(false);
   useEffect(() => {
     setUserToken(userToken);
   }, []);
+
+  const renderMenuItems = () => {
+    if (userType === 'BUYER') {
+      return (
+        <MenuItem
+          onClick={() => navigate('/cart')}
+          src="/assets/icon-shopping-cart.png"
+          alt="장바구니 이미지"
+          text="장바구니"
+        />
+      );
+    }
+    if (userToken) {
+      return (
+        <li className="listLi mypage" onClick={() => setModalOpen(!isModalOpen)}>
+          <img
+            className="navImg"
+            src="/assets/icon-user.svg"
+            alt="로그인된 유저 이미지"
+          />
+          <p>마이페이지</p>
+          {isModalOpen && <MyPageModal />}
+        </li>
+      );
+    }
+
+    if (userType === 'SELLER') {
+      return (
+        <li className="shoppingBagLi" onClick={() => navigate('/seller-center')}>
+          <img src="/assets/icon-shopping-bag.png" alt="판매자 센터 이미지" />
+          <p>판매자센터</p>
+        </li>
+      );
+    }
+
+    if (!userToken) {
+      return (
+        <MenuItem
+          onClick={() => navigate('/login')}
+          src="/assets/icon-user.svg"
+          alt="유저 이미지"
+          text="로그인"
+        />
+      );
+    }
+  };
   return (
-    <WrapperDiv className="wrapper-nav">
-      <WrapperNav>
-        <LogoDiv className="logo-container">
-          <LogoImg
+    <WrapperDiv>
+      <nav className="wrapperNav">
+        <div className="logoBoxDiv">
+          <img
+            className="logoImg"
             onClick={() => {
               navigate('/');
             }}
             src="/assets/Logo-hodu.png"
             alt="메인로고"
           />
-          <SearchInput type="text" placeholder="상품을 검색해보세요!" />
-          <SearchImg src="/assets/icon-search.svg" alt="검색 이미지" />
-        </LogoDiv>
-
-        <NavListUl className="nav-right-list">
-          <NavListLi onClick={() => navigate('/cart')}>
-            <NavListImg src="/assets/icon-shopping-cart.svg" alt="장바구니 이미지" />
-            <NavListP>장바구니</NavListP>
-          </NavListLi>
-          {userToken ? (
-            <NavListLi onClick={() => setModalOpen(!isModalOpen)}>
-              <NavListImg src="/assets/icon-user.svg" alt="로그인된 유저 이미지" />
-              <NavListP>마이페이지</NavListP>
-            </NavListLi>
-          ) : (
-            <NavListLi onClick={() => navigate('/login')}>
-              <NavListImg src="/assets/icon-user.svg" alt="유저 이미지" />
-              <NavListP>로그인</NavListP>
-            </NavListLi>
-          )}
-          {isModalOpen && <MyPageModal />}
-        </NavListUl>
-      </WrapperNav>
+          <input className="searchInput" type="text" placeholder="상품을 검색해보세요!" />
+          <img className="searchImg" src="/assets/icon-search.svg" alt="검색 이미지" />
+        </div>
+        <ul className="listUl">{renderMenuItems()}</ul>
+      </nav>
     </WrapperDiv>
   );
 };
@@ -54,74 +91,85 @@ export default Nav;
 
 const WrapperDiv = styled.nav`
   box-shadow: 0px 2px 5px 0px rgba(0, 0, 0, 0.3);
-`;
+  .wrapperNav {
+    max-width: 65vw;
+    display: flex;
+    margin: 0 auto;
+    justify-content: space-between;
+    padding: 26px 0px;
 
-const WrapperNav = styled.nav`
-  max-width: 65vw;
-  display: flex;
-  margin: 0 auto;
-  justify-content: space-between;
-  padding: 26px 0px;
-`;
+    .logoBoxDiv {
+      width: 700px;
+      display: flex;
+      gap: 50px;
+      align-items: center;
+      position: relative;
 
-const LogoDiv = styled.div`
-  width: 700px;
-  display: flex;
-  align-items: center;
-  position: relative;
-`;
-
-const LogoImg = styled.img`
-  width: 200px;
-  object-fit: contain;
-`;
-
-const SearchInput = styled.input`
-  width: 450px;
-  height: 60px;
-  outline: none;
-  font-weight: 400;
-  font-size: 25px;
-  color: var(--sub-text-color);
-  margin-left: 50px;
-  padding: 20px;
-
-  background: #ffffff;
-  border: 4px solid var(--main-color);
-  border-radius: 50px;
-`;
-
-const SearchImg = styled.img`
-  position: absolute;
-  width: 28px;
-  height: 28px;
-  right: 22px;
-  top: 22px;
-  cursor: pointer;
-`;
-
-const NavListUl = styled.ul`
-  display: flex;
-  justify-content: flex-end;
-  width: 100%;
-  max-width: 450px;
-  gap: 15px;
-  position: relative;
-`;
-
-const NavListLi = styled.li`
-  display: flex;
-  width: 120px;
-  flex-direction: column;
-  align-items: center;
-  cursor: pointer;
-`;
-
-const NavListP = styled.p`
-  font-size: 20px;
-`;
-
-const NavListImg = styled.img`
-  width: 50px;
-  margin-bottom: 5px;
+      .logoImg {
+        width: 200px;
+        object-fit: contain;
+      }
+      .searchInput {
+        width: 100%;
+        height: 100%;
+        padding: 0 30px;
+        outline: none;
+        font-size: 25px;
+        display: flex;
+        background: #ffffff;
+        border: 4px solid var(--main-color);
+        border-radius: 50px;
+      }
+      .searchImg {
+        position: absolute;
+        width: 36px;
+        top: 0;
+        right: 30px;
+        cursor: pointer;
+      }
+    }
+    .listUl {
+      display: flex;
+      justify-content: flex-end;
+      width: 100%;
+      max-width: 450px;
+      gap: 15px;
+      position: relative;
+    }
+    .listLi {
+      display: flex;
+      width: 120px;
+      flex-direction: column;
+      align-items: center;
+      cursor: pointer;
+      .navImg {
+        width: 50px;
+        margin-bottom: 5px;
+      }
+      p {
+        font-size: 20px;
+      }
+    }
+    .listLi.mypage {
+      position: relative;
+    }
+    .shoppingBagLi {
+      width: 168px;
+      border-radius: 5px;
+      background-color: var(--main-color);
+      color: white;
+      display: flex;
+      gap: 8px;
+      font-size: 24px;
+      justify-content: center;
+      align-items: center;
+      cursor: pointer;
+      img {
+        width: 32px;
+        height: 32px;
+      }
+      p {
+      }
+    }
+  }
 `;
