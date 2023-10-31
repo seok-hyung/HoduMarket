0b31819830e32f988275fa6c0895db8d59f13ccc
seok-hyung <korea970223@naver.com>
2023-10-31 19:12:39 +0900

    :recycle:Refactor : 캐러셀 이미지 리스트 외부 파일로 분리

---
+++ src/components/common/carousel/ImgList.ts
@@ -0,0 +1,27 @@
+export type Image = {
+  id: number;
+  url: string;
+};
+
+export const imgList: Image[] = [
+  {
+    id: 1,
+    url: 'https://images.unsplash.com/photo-1587731556938-38755b4803a6?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2978&q=80',
+  },
+  {
+    id: 2,
+    url: 'https://images.unsplash.com/photo-1508272849285-ec94ff3855e2?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2940&q=80',
+  },
+  {
+    id: 3,
+    url: 'https://images.unsplash.com/photo-1543622748-5ee7237e8565?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2970&q=80',
+  },
+  {
+    id: 4,
+    url: 'https://images.unsplash.com/photo-1603917745459-0078218e7c5c?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2940&q=80',
+  },
+  {
+    id: 5,
+    url: 'https://images.unsplash.com/photo-1680059439144-aefe88d4c858?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=3024&q=80',
+  },
+];
