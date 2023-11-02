23ca29b499346ee370842bae51bfa24b81646f25
seok-hyung <korea970223@naver.com>
2023-11-02 22:52:15 +0900

    :sparkles:Feat : 판매자센터용 Nav 컴포넌트 추가

---
+++ src/components/common/nav/SellerNav.tsx
@@ -0,0 +1,44 @@
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
+import styled from 'styled-components';
+
+const SellerNav = () => {
+  const navigate = useNavigate();
+  return (
+    <WrapperDiv>
+      <nav className="wrapperNav">
+        <img
+          onClick={() => {
+            navigate('/');
+          }}
+          src="/assets/Logo-hodu.png"
+          alt="메인로고"
+        />
+        <p>판매자 센터</p>
+      </nav>
+    </WrapperDiv>
+  );
+};
+
+export default SellerNav;
+
+const WrapperDiv = styled.nav`
+  box-shadow: 0px 2px 5px 0px rgba(0, 0, 0, 0.3);
+  .wrapperNav {
+    max-width: 90vw;
+    display: flex;
+    margin: 0 auto;
+    padding: 26px 0px;
+    gap: 30px;
+    img {
+      width: 200px;
+    }
+    p {
+      font-size: 44px;
+      font-weight: bold;
+      line-height: 40px;
+      display: flex;
+      align-items: center;
+    }
+  }
+`;
