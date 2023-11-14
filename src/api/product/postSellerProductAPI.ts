8b3345bfa37b9ef39e99462808136daf33a621bc
seok-hyung <korea970223@naver.com>
2023-11-15 04:51:15 +0900

    :bug:Modify : 상품업로드API 에러 해결 #27

--- src/api/product/postSellerProductAPI.ts
+++ src/api/product/postSellerProductAPI.ts
@@ -1,30 +1,32 @@
 import { apiURL } from 'api/apiURL';
+import { PostSellerProductForm } from 'model/market';
 
 export const postSellerProductAPI = async (
   token: string,
   formData: PostSellerProductForm,
 ) => {
-  const res = await fetch(`${apiURL}/products`, {
+  // const data = new FormData();
+  // for (const key in formData) {
+  //   if (Object.prototype.hasOwnProperty.call(formData, key)) {
+  //     const element = formData[key as keyof PostSellerProductForm];
+  //     data.append(key, element);
+  //   }
+  // }
+  const data = new FormData();
+  Object.entries(formData).forEach(([key, value]) => {
+    data.append(key, value);
+  });
+  const res = await fetch(`${apiURL}/products/`, {
     method: 'POST',
-    body: JSON.stringify(formData),
+    body: data,
     headers: {
       Authorization: `JWT ${token}`,
-      'Content-type': 'application/json',
+      // 'Content-type': 'application/json',
     },
   });
   if (!res.ok) {
     throw new Error('네트워크에 문제가 있습니다.');
   }
-  const data = await res.json();
-  return data;
-};
-
-type PostSellerProductForm = {
-  product_name: string;
-  image: File;
-  price: number;
-  shipping_method: 'PARCEL' | 'DELIVERY';
-  shipping_fee: number;
-  stock: number;
-  product_info: string;
+  const result = await res.json();
+  return result;
 };
