f57783a7fef8c50590d998fbd8ad70ef0ccaeec6
seok-hyung <korea970223@naver.com>
2023-11-14 23:46:35 +0900

    :lipstick:Modify : 툴바 스타일 변경 및 버튼 삭제

--- src/components/common/textEditor/TextEditor.tsx
+++ src/components/common/textEditor/TextEditor.tsx
@@ -1,22 +1,25 @@
 import React, { useState } from 'react';
 import { Editor } from 'react-draft-wysiwyg';
-import { EditorState, convertToRaw } from 'draft-js';
+import { EditorState } from 'draft-js';
 import draftToHtml from 'draftjs-to-html';
 import 'react-draft-wysiwyg/dist/react-draft-wysiwyg.css';
 import styled from 'styled-components';
 
-const TextEditor = () => {
-  const [editorState, setEditorState] = useState(() => EditorState.createEmpty());
+type TextEditorProps = {
+  value: string;
+  onChange: (value: string) => void;
+};
 
-  const onEditorStateChange = (editorState: any) => {
-    setEditorState(editorState);
-  };
+const TextEditor = ({ value, onChange }: TextEditorProps) => {
+  const contentState = EditorState.createEmpty().getCurrentContent();
+  const [editorState, setEditorState] = useState(
+    EditorState.createWithContent(contentState),
+  );
 
-  const saveContent = () => {
-    const rawContentState = convertToRaw(editorState.getCurrentContent());
-    const markup = draftToHtml(rawContentState);
-    console.log(markup);
-    // 이 markup을 원하는 곳에 저장하거나 사용하면 됩니다.
+  const onEditorStateChange = (newEditorState: EditorState) => {
+    setEditorState(newEditorState);
+    const html = newEditorState.getCurrentContent().getPlainText();
+    onChange(html);
   };
 
   return (
@@ -28,56 +31,29 @@ const TextEditor = () => {
         editorClassName="editor-class"
         toolbarClassName="toolbar-class"
       />
-      <BtnDiv>
-        <button className="cancel">취소</button>
-        <button className="save" onClick={saveContent}>
-          저장
-        </button>
-      </BtnDiv>
     </Wrapper>
   );
 };
 
 export default TextEditor;
+
 const Wrapper = styled.div`
   .wrapper-class {
-    width: 100%; /* 컨테이너의 너비를 100%로 설정 */
+    width: 100%;
     border: 1px solid #c4c4c4;
     border-radius: 5px;
   }
 
   .editor-class {
-    min-height: 500px; /* 에디터 부분의 높이를 500px로 설정 */
+    min-height: 600px;
   }
   .editor-class::-webkit-scrollbar {
     display: none;
   }
 
+  /* 툴바 스타일 지정 */
   .toolbar-class {
-    /* 툴바 스타일 지정 */
-    height: 70px; /* 툴바의 높이 설정 */
-    padding: 15px; /* 툴바 내부의 패딩 설정 */
-    gap: 20px;
+    padding: 10px; /* 툴바 내부의 패딩 설정 */
     border-radius: 5px;
-  }
-`;
-const BtnDiv = styled.div`
-  display: flex;
-  gap: 30px;
-  justify-content: end;
-  button {
-    width: 220px;
-    padding: 20px;
-    font-size: 24px;
-    border-radius: 5px;
-  }
-  .cancel {
-    border: 1px solid #e4e4e4;
-  }
-  .save {
-    background-color: var(--main-color);
-    width: 220px;
-    color: white;
-    padding: 20px;
   }
 `;
