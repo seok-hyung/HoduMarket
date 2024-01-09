b58696fb174a8a92d86c0f2502ab41c3007646ed
seok-hyung <korea970223@naver.com>
2024-01-10 01:31:31 +0900

    :fire:Delete : 불필요한 코드 제거

--- src/api/product/postSellerProductAPI.ts
+++ src/api/product/postSellerProductAPI.ts
@@ -5,13 +5,6 @@ export const postSellerProductAPI = async (
   token: string,
   formData: PostSellerProductForm,
 ) => {
-  // const data = new FormData();
-  // for (const key in formData) {
-  //   if (Object.prototype.hasOwnProperty.call(formData, key)) {
-  //     const element = formData[key as keyof PostSellerProductForm];
-  //     data.append(key, element);
-  //   }
-  // }
   const data = new FormData();
   Object.entries(formData).forEach(([key, value]) => {
     data.append(key, value);
@@ -21,11 +14,11 @@ export const postSellerProductAPI = async (
     body: data,
     headers: {
       Authorization: `JWT ${token}`,
-      // 'Content-type': 'application/json',
     },
   });
   if (!res.ok) {
-    throw new Error('네트워크에 문제가 있습니다.');
+    const error = await res.json();
+    console.log(error);
   }
   const result = await res.json();
   return result;
