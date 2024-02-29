acfd41242e8a633d918e48a41dca20915b168457
seok-hyung <korea970223@naver.com>
2024-02-29 21:17:07 +0900

    :recycle:Modify : 캐러셀 이미지 최적화된 이미지로 변경

--- src/components/common/carousel/ImgList.ts
+++ src/components/common/carousel/ImgList.ts
@@ -6,22 +6,22 @@ export type Image = {
 export const imgList: Image[] = [
   {
     id: 1,
-    url: 'https://images.unsplash.com/photo-1587731556938-38755b4803a6?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2978&q=80',
+    url: '/assets/carousel1_11zon.webp',
   },
   {
     id: 2,
-    url: 'https://images.unsplash.com/photo-1508272849285-ec94ff3855e2?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2940&q=80',
+    url: '/assets/carousel2_11zon.webp',
   },
   {
     id: 3,
-    url: 'https://images.unsplash.com/photo-1543622748-5ee7237e8565?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2970&q=80',
+    url: '/assets/carousel3_11zon.webp',
   },
   {
     id: 4,
-    url: 'https://images.unsplash.com/photo-1603917745459-0078218e7c5c?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2940&q=80',
+    url: '/assets/carousel4_11zon.webp',
   },
   {
     id: 5,
-    url: 'https://images.unsplash.com/photo-1680059439144-aefe88d4c858?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=3024&q=80',
+    url: '/assets/carousel5_11zon.webp',
   },
 ];
