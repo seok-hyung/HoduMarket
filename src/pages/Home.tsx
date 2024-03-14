cf6f6df532eb55224aaccfef1191340013dc6fd7
seok-hyung <korea970223@naver.com>
2024-03-14 21:13:10 +0900

    :gear:Add : lazysizes 라이브러리 설치

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 
 // 컴포넌트
 import Nav from 'components/common/nav/Nav';
@@ -6,26 +6,34 @@ import Carousel from 'components/common/carousel/Carousel';
 import Products from 'components/common/products/Products';
 import Footer from 'components/common/footer/Footer';
 import Pagination from 'components/common/pagination/Pagiation';
+import ScrollToTopBtn from 'components/common/scrollToTopBtn/ScrollToTopBtn';
+import Skeleton from 'components/common/skeleton/Skeleton';
 
 // 커스텀 훅
 import { useGetProducts } from 'hooks/UseGetProducts';
-import ScrollToTopBtn from 'components/common/scrollToTopBtn/ScrollToTopBtn';
-import Skeleton from 'components/common/skeleton/Skeleton';
+import { getSearchProducstAPI } from 'api/product/getSearchProductsAPI';
 
 const Home = () => {
   const [currentPage, setCurrentPage] = useState(1);
   const [searchValue, setSearchValue] = useState('');
-  const { products } = useGetProducts(); // 전체 상품 데이터를 가져옵니다.
+  const { products } = useGetProducts(); // 전체 상품 데이터 가져오기
 
   const filteredProducts = products.filter((product) =>
     product.product_name.includes(searchValue),
   );
-  const totalPage = Math.ceil(filteredProducts.length / 15);
-  // 현재 페이지에 해당하는 상품들만 선택합니다.
+  useEffect(() => {
+    getSearchProducstAPI(searchValue).then((res) => {
+      console.log(res);
+    });
+  }, [searchValue]);
+  const MAX_PRODUCTS_PER_PAGE = 15; // 페이지당 최대 상품 수 명시
+  const totalPage = Math.ceil(filteredProducts.length / MAX_PRODUCTS_PER_PAGE);
+  // 현재 페이지에 해당하는 상품들만 선택
   const paginatedProducts = filteredProducts.slice(
-    (currentPage - 1) * 15,
-    currentPage * 15,
+    (currentPage - 1) * MAX_PRODUCTS_PER_PAGE,
+    currentPage * MAX_PRODUCTS_PER_PAGE,
   );
+
   return (
     <>
       <Nav setSearchValue={setSearchValue} />
@@ -33,7 +41,7 @@ const Home = () => {
       {products.length > 0 ? (
         <Products products={paginatedProducts} />
       ) : (
-        <Skeleton count={15} />
+        <Skeleton count={MAX_PRODUCTS_PER_PAGE} />
         // <p>Loading...</p>
       )}
       <Pagination
