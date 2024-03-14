bb40dc2ca9afd73c6cd621d5a6da87be6f85c0ad
seok-hyung <korea970223@naver.com>
2024-03-14 21:14:49 +0900

    :fire:Delete : 불필요한 코드 삭제

--- src/api/product/getAllProductsAPI.ts
+++ src/api/product/getAllProductsAPI.ts
@@ -7,8 +7,7 @@ export const getAllProductsAPI = async (page: number) => {
     if (!response.ok) {
       throw new Error('네트워크에 문제가 있습니다.');
     }
-    const data = await response.json();
-    return data;
+    return response.json();
   } catch (error) {
     console.error('상품 데이터를 가져오는데 문제가 있습니다.', error);
   }
--- src/api/product/getSearchProductsAPI.ts
+++ src/api/product/getSearchProductsAPI.ts
@@ -7,8 +7,7 @@ export const getSearchProducstAPI = async (searchValue: string) => {
     if (!res.ok) {
       throw new Error('네트워크에 문제가 있습니다.');
     }
-    const data = res.json();
-    return data;
+    return res.json();
   } catch (error) {
     console.log('상품 검색 데이터를 가져오는데 문제가 있습니다.', error);
   }
