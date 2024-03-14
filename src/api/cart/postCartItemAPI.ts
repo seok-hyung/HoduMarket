c272c3fc171a47d04a57c0fbaf81747f0900a2c0
seok-hyung <korea970223@naver.com>
2024-03-14 21:14:27 +0900

    :recycle:Modify : 자세한 에러 메세지 출력 가능하게 수정

--- src/api/cart/postCartItemAPI.ts
+++ src/api/cart/postCartItemAPI.ts
@@ -14,10 +14,11 @@ export const postCartItemAPI = async (
   });
 
   if (!res.ok) {
+    // console.log(`Error: ${res.status} ${res.statusText}`);
     throw new Error('네트워크에 문제가 있습니다.');
   }
   const data = await res.json();
-  console.log(data);
+  // console.log(data);
 
   return data;
 };
